e411b6b370122898082ff4e4f0e64e44d2119e37
ThusEndingTheStory <102835906+ThusEndingTheStory@users.noreply.github.com>
2022-06-16 14:34:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'Admin_List_BETA_v01',         # How you named your package folder (MyLib)
+  packages = ['Admin_List_BETA_v01'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  description = 'For administration things (wow I sound unprofessional)',   # Give a short description about your library
+  author = 'ThusEndingTheStory',                   # Type in your name
+  author_email = 'Not gonna give it',      # Type in your E-Mail
+  url = '',   # Provide either the link to your github or to your website
+  download_url = 'idk',    # I explain this later on - https://github.com/user/reponame/archive/v_01.tar.gz
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
           'beautifulsoup4',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
